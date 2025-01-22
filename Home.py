15093b517af887ee5fb0c0fe1ffd1cd856acd1af
Dehkartes <hsj3925@naver.com>
2025-01-22 23:40:06 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,3 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 # Main page
 st.set_page_config(page_title="Streamlit Multi-Page App")
+
+st.title("Welcome to the Fractarium")
+st.write("Auto PR Agent[WIP]: This is a tool that helps you to automate the process of creating a pull request.\n\n")
+st.write("About Me: The RAG chatbot that introduces me")
